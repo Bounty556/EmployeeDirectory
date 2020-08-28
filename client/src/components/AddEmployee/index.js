e734a7a32ad6873f4028139457c3520267263a9a
Bounty556 <jacobmayday@gmail.com>
2020-08-28 12:55:07 -0600

    Added basic AddEmployee layout

--- client/src/components/AddEmployee/index.js
+++ client/src/components/AddEmployee/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function AddEmployee() {
-  return <div>Add Employee</div>;
+  return (
+    <div>
+      <form>
+
+        <label for='addEmployeeName'>Name:</label>
+        <input type='text' id='addEmployeeName' placeholder='Susan Bog' />
+
+        <label for='addEmployeeOccupation'>Occupation:</label>
+        <input type='text' id='addEmployeeOccupation' placeholder='Sales Manager' />
+        
+        <label for='addEmployeeDepartment'>Department:</label>
+        <input type='text' id='addEmployeeDepartment' placeholder='Sales' />
+
+        <label for='addEmployeeSalary'>Salary:</label>
+        <input type='number' id='addEmployeeSalary' placeholder='120000' />
+
+        <input type='submit' value='Add Employee' />
+      </form>
+    </div>
+  );
 }
 
 export default AddEmployee;
