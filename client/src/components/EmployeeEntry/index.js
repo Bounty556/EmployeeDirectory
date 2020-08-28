3989970f98546b399a8028b5f4e57edd8ce8acd5
Bounty556 <jacobmayday@gmail.com>
2020-08-28 13:03:30 -0600

    Added EmployeeEntry component

---
+++ client/src/components/EmployeeEntry/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EmployeeEntry() {
+  return (<div>
+    <p>Name: name</p>
+    <p>Occupation: occupation</p>
+    <p>Department: department</p>
+    <p>Salary: $100,000</p>
+  </div>)
+};
+
+export default EmployeeEntry;
